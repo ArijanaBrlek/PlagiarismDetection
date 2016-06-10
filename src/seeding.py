de233840660158b8056e3989a4be421c58ce680c
Nino Uzelac <uzelac.nino@gmail.com>
2016-06-10 14:51:38 +0200

    Test with updated alzahrani similarity

--- src/seeding.py
+++ src/seeding.py
@@ -1,22 +1,26 @@
 class Seeding(object):
 
     @staticmethod
-    def alzahrani_similarity(sentence_a, sentence_b, model):
-        similarity = 0
-        for word_a in sentence_a:
-            if word_a in sentence_b:
-                similarity += 1
-            else:
-                max_sim = 0
-                for word_b in sentence_b:
-                    if word_a in model and word_b in model:
-                        sim = model.similarity(word_a, word_b);
-                        if sim > max_sim:
-                            max_sim = sim
-                similarity += max_sim
-        length = max(len(sentence_a), len(sentence_b))
-        if length != 0:
-            res = similarity / length
-        else:
-            res = 0
-        return res
+    def alzahrani_similarity(s1, s2, model):
+        string_similarity = []
+        for ws1 in s1:
+            max_word_similarity = 0
+            max_similar_word    = u""
+            for ws2 in s2:
+                if(ws1 in model and ws2 in model):
+                    similarity = model.similarity(ws1, ws2)
+                    if similarity > max_word_similarity:
+                        max_word_similarity = similarity
+                        max_similar_word    = ws2
+            if max_word_similarity > 0:
+                string_similarity.append( max_word_similarity )
+        try:
+            return sum(string_similarity) / len(string_similarity)
+        except Exception as exc:
+            print exc
+            for x in s1:
+                print "".join(y for y in x if ord(y) < 128),
+            for x in s2:
+                print "".join(y for y in x if ord(y) < 128),
+            print "\n"
+            return 0
